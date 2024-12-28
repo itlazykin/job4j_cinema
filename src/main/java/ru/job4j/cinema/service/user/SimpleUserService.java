4c88f18006f8fab5acdf9065095b5d80dfeefc02
itlazykin <dzaka89@gmail.com>
2024-12-28 14:52:50 +0300

    Исправление User сервиса.

--- src/main/java/ru/job4j/cinema/service/user/SimpleUserService.java
+++ src/main/java/ru/job4j/cinema/service/user/SimpleUserService.java
@@ -1,10 +1,14 @@
 package ru.job4j.cinema.service.user;
 
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Service;
 import ru.job4j.cinema.model.User;
 import ru.job4j.cinema.repository.user.UserRepository;
 
 import java.util.Optional;
 
+@Service
+@ThreadSafe
 public class SimpleUserService implements UserService {
     private UserRepository userRepository;
 
